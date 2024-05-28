1c0d76640b880fe6abcd431ed2fab09ece158680
Tal Rosenberger <tal.rosenberger@gmail.com>
2024-05-28 18:48:04 +0400

    Update index.js

--- index.js
+++ index.js
@@ -12,27 +12,31 @@ var DOMAIN_MANAGER = core.getInput('domain-manager')
 async function installServerlessAndPlugins() {
   await exeq(
     `echo Installing Serverless and plugins...`,
-    `npm i serverless -g`,
+    //`npm i serverless -g`,
+    `npm i serverless@3.21.0 -g`,
     `npm i serverless-plugin-canary-deployments`,
-    `npm i serverless-python-requirements`
+    //`npm i serverless-python-requirements`
+    `npm i serverless-python-requirements@5.4.0`
   )
 }
 
-//  Runs Serverless deploy using AWS Credentials if specified, else SERVERLESS ACCESS KEY
+//  Runs Serverless deployment using AWS Credentials if specified, else SERVERLESS ACCESS KEY
 async function runServerlessDeploy() {
   await exeq(
     `echo Running sls deploy...`,
     `if [ ${process.env.AWS_ACCESS_KEY_ID} ] && [ ${process.env.AWS_SECRET_ACCESS_KEY} ]; then
-      sls config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} --verbose
+      // sls config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} --verbose
+      npx sls config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} --verbose
     fi`,
-    `sls deploy --verbose`
+    //`sls deploy --verbose`
+    `npx sls deploy --verbose`
   )
 }
 
 //  Runs all functions sequentially
 async function handler() {
   try {
-    await installServerlessAndPlugins()
+    // await installServerlessAndPlugins()
     await runServerlessDeploy()
   } catch (error) {
     core.setFailed(error.message);
